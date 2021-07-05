202b46a45fd69183a559c25db23efb52c3303405
seiei <moriatyseiei@hotmail.com>
2021-07-05 11:00:24 +0800

    updated

--- seiei/algo/Solution.java
+++ seiei/algo/Solution.java
@@ -3,29 +3,65 @@ package algo;
 import java.util.*;
 
 class Solution {
-    public long wonderfulSubstrings(String word) {
-        int[] map = new int[1024];
-        map[0] = 1;
-        int mask = 0;
-        long count = 0;
-        for (char c: word.toCharArray()) {
-            mask ^= 1 << c - 'a';
-            count += map[mask];
-            for (int i = 0; i < 10; i++) {
-                count += map[mask ^ 1 << i];
+    Map<String, Integer> map = new HashMap<>();
+    public String countOfAtoms(String formula) {
+        int len = formula.length();
+        int start = 0;
+        Stack<Integer> quotes = new Stack<>();
+        for (int i = 0; i < len; i++) {
+            if (formula.charAt(i) == '(') {
+                count(formula, start, i - 1, 1);
+                start = i + 1;
+            } else if (formula.charAt(i) == ')') {
+                int numEnd = i + 1;
+                while (numEnd < len && numEnd < 65) numEnd++;
+                count(formula, start, i - 1, Integer.valueOf(formula.substring(i + 1, numEnd)));
             }
-            map[mask]++;
+
+            if (formula.charAt(i) == '(') {
+                if (quotes.isEmpty()) {
+                    count(formula, start, i - 1, 1);
+                    start = i + 1;
+                }
+                quotes.add(i + 1);
+            } else if (formula.charAt(i) == ')') {
+
+            }
         }
-        return count;
+        if (formula.charAt(len - 1) >= 65) count(formula, start, len - 1, 1);
+
+        // 从map转为String
+        StringBuilder ans = new StringBuilder();
+        for (Map.Entry<String, Integer> entry: map.entrySet()) {
+            ans.append(entry.getKey());
+            ans.append(entry.getValue() == 1 ? "" : entry.getValue());
+        }
+        return ans.toString();
     }
 
-    public static void main(String[] args) {
-        int a = -12;
-        int num = 0;
-        while (a != 0) {
-            num = num * 10 + a % 10;
-            a /= 10;
+    // 严格没有括号
+    private void count(String s, int startIndex, int endIndex, int bonus) {
+        StringBuilder atom = null;
+        StringBuilder num = null;
+        for (int i = startIndex; i <= endIndex; i++) {
+            char c = s.charAt(i);
+            if (c < 65) {
+                num = new StringBuilder();
+                num.append(c);
+            } else if (c < 97) {
+                if (atom != null) {
+                    addToMap(atom.toString(), (num == null ? 1 : Integer.valueOf(num.toString())) * bonus);
+                }
+                atom = new StringBuilder();
+                num = null;
+                atom.append(c);
+            } else atom.append(c);
         }
-        System.out.println(num);
+        addToMap(atom.toString(), (num == null ? 1 : Integer.valueOf(num.toString())) * bonus);
+    }
+
+    private void addToMap(String atom, int num) {
+        if (!map.containsKey(atom))  map.put(atom, num);
+        else map.put(atom, map.get(map) + num);
     }
 }
