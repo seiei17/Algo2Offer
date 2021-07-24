614abaf41eefc1a9b18e9dd1559a61ad89b645e1
seiei <moriatyseiei@hotmail.com>
2021-07-24 10:00:53 +0800

    20210724

--- seiei/algo/Solution.java
+++ seiei/algo/Solution.java
@@ -17,55 +17,7 @@ public class Solution {
 
     @Test
     public void test() {
-        ExecutorService es = Executors.newCachedThreadPool();
-        Sample s = new Sample();
-        es.execute(s::t2);
-        es.execute(s::t1);
-        es.execute(s::wake);
+
     }
 
-}
-
-class Sample {
-    private Lock lock = new ReentrantLock();
-    private Condition con1 = lock.newCondition();
-    private Condition con2 = lock.newCondition();
-
-    public void wake() {
-        lock.lock();
-        try {
-            System.out.println("Now wake t1");
-            con1.signal();
-            System.out.println("Now wake t2");
-            con2.signal();
-        } finally {
-            lock.unlock();
-        }
-    }
-
-    public void t1() {
-        lock.lock();
-        try {
-            System.out.println("t1 await");
-            con1.await();
-            System.out.println("t1 woken");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            lock.unlock();
-        }
-    }
-
-    public void t2() {
-        lock.lock();
-        try {
-            System.out.println("t2 await");
-            con2.await();
-            System.out.println("t2 woken");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            lock.unlock();
-        }
-    }
 }
