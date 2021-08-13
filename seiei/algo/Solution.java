814b508b1765ef281ed2d39ec67c1e9eb8c1175b
moriatyseiei <moriatyseiei@hotmail.com>
2021-08-13 22:17:31 +0800

    '20210813'

--- seiei/algo/Solution.java
+++ seiei/algo/Solution.java
@@ -2,35 +2,33 @@ package algo;
 
 import org.junit.Test;
 
+import java.util.*;
+
 public class Solution {
     @Test
     public void test() {
-        int[] nums = {10,14,49,22,7,6,25};
-        int k = 2;
-        System.out.println(minSpaceWastedKResizing(nums, k));
-    }
-    private int ans;
-    public int minSpaceWastedKResizing(int[] nums, int k) {
-        int n = nums.length;
-        ans = Integer.MAX_VALUE;
-        for (int num : nums) {
-            cal(nums, k, n, num, 0, 0);
-        }
-        return ans;
+        int[] nums = {0,0,1,2,5};
+        System.out.println(isStraight(nums));
     }
 
-    private void cal(int[] nums, int k, int n, int capacity, int waste, int idx) {
-        if (idx == n) {
-            ans = Math.min(ans, waste);
-            return;
+    public boolean isStraight(int[] nums) {
+        int[] cnt = new int[14];
+        for (int n: nums) cnt[n]++;
+        boolean begin = false;
+        int number = 0;
+        for (int i = 1; i <= 13; i++) {
+            if (begin) {
+                if (cnt[i] == 0) {
+                    if (cnt[0] > 0) {
+                        cnt[0]--;
+                    } else return false;
+                }
+                if (++number == 5) return true;
+            } else if (nums[i] > 0) {
+                begin = true;
+                number++;
+            }
         }
-        if (nums[idx] == capacity) {
-            cal(nums, k, n, capacity, waste, idx + 1);
-        } else if (nums[idx] < capacity) {
-            if (k > 0) cal(nums, k - 1, n, nums[idx], waste, idx + 1);
-            cal(nums, k, n, capacity, waste - nums[idx] + capacity, idx + 1);
-        } else {
-            if (k > 0) cal(nums, k - 1, n, nums[idx], waste, idx + 1);
-        }
+        return number == 5;
     }
 }
